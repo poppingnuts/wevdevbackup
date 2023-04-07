fbf2951dbfc812741aa416ee2d6d3a0c896a50c1
poppingnuts <poppingnuts0@gmail.com>
2023-04-08 02:51:32 +0530

    whts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 import { motion } from "framer-motion";
-
-
-import {
-  About,
-  Contact,
-  Experience,
-  
-  Hero,
-  Navbar,
-  
-  Works,
-  StarsCanvas,
-  EarthCanvas,
-  
-} from "./components";
-
+import { About, Contact, Experience, Hero, Navbar, Works, StarsCanvas, EarthCanvas } from "./components";
 import { slideIn } from "./utils/motion";
 import { whtsapp } from "./assets";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -28,23 +14,22 @@ const App = () => {
         </div>
         <About />
         <Experience />
-      
         <Works />
-        
         <div className="relative z-0">
-        <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
-      >
-        <EarthCanvas />
-      </motion.div>
-        
+          <motion.div
+            variants={slideIn("right", "tween", 0.2, 1)}
+            className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
+          >
+            <EarthCanvas />
+          </motion.div>
           <Contact />
           <StarsCanvas />
-
         </div>
-        <div class="sticky top-0 ..."><a href="https://wa.me/+918088563651"><img src={whtsapp} alt="whtsapp" className="w-16 h-16 stic object-contains" /></a></div>
-        
+        <div style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
+          <a href="https://wa.me/+918088563651">
+            <img src={whtsapp} alt="whtsapp" className="w-16 h-16 stic object-contains" />
+          </a>
+        </div>
       </div>
     </BrowserRouter>
   );
